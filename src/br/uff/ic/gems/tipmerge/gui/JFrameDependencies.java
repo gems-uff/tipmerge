56d4d9e401fd16238d132c143edeec992880b718
José Jair Cavalcante de Figueirêdo <jairfigueiredo@gmail.com>
2015-05-16 22:18:05 -0500

    Generating files dependencies matrix from Dominoes

--- src/br/uff/ic/gems/tipmerge/gui/JFrameDependencies.java
+++ src/br/uff/ic/gems/tipmerge/gui/JFrameDependencies.java
@@ -5,15 +5,25 @@
  */
 package br.uff.ic.gems.tipmerge.gui;
 
+import arch.Cell;
+import arch.IMatrix2D;
+import arch.MatrixDescriptor;
+import br.uff.ic.gems.tipmerge.dao.EditedFilesDao;
 import br.uff.ic.gems.tipmerge.dao.MergeCommitsDao;
+import br.uff.ic.gems.tipmerge.dao.MergeFilesDao;
+import br.uff.ic.gems.tipmerge.model.EditedFile;
 import br.uff.ic.gems.tipmerge.model.Merge;
 import br.uff.ic.gems.tipmerge.model.MergeCommits;
+import br.uff.ic.gems.tipmerge.model.MergeFiles;
 import br.uff.ic.gems.tipmerge.model.Repository;
 import dao.DominoesSQLDao2;
 import domain.Dominoes;
 import java.io.File;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ComboBoxModel;
@@ -56,7 +66,11 @@ public class JFrameDependencies extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         mergesList = new javax.swing.JComboBox();
+        spinnerThreshold = new javax.swing.JSpinner();
+        jLabel2 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Dependencies Analysis");
@@ -80,6 +94,10 @@ public class JFrameDependencies extends javax.swing.JFrame {
             }
         });
 
+        spinnerThreshold.setModel(new javax.swing.SpinnerNumberModel(0.5d, 0.0d, 1.0d, 0.1d));
+
+        jLabel2.setText("Threshold:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -87,16 +105,21 @@ public class JFrameDependencies extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 271, Short.MAX_VALUE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(mergesList, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(spinnerThreshold, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1)))
+                        .addComponent(jButton1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(mergesList, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE))))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -106,26 +129,40 @@ public class JFrameDependencies extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel3)
-                        .addComponent(mergesList, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(mergesList, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(spinnerThreshold, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1)
+                .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 213, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 213, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -167,14 +204,40 @@ public class JFrameDependencies extends javax.swing.JFrame {
 		
 		try {
 			System.out.println("\nCreating the dominoes of Branch One");
-			List<Dominoes> dominoesBranchOne = DominoesSQLDao2.loadAllMatrices(databaseName, jTextField1.getText(), "CPU", branchOne);
+			//List<Dominoes> dominoesBranchOne = DominoesSQLDao2.loadAllMatrices(databaseName, jTextField1.getText(), "CPU", branchOne);
 
 			System.out.println("\nCreating the dominoes of branch Two");
-			List<Dominoes> dominoesBranchTwo = DominoesSQLDao2.loadAllMatrices(databaseName, jTextField1.getText(), "CPU", branchTwo);
+			//List<Dominoes> dominoesBranchTwo = DominoesSQLDao2.loadAllMatrices(databaseName, jTextField1.getText(), "CPU", branchTwo);
 
 			System.out.println("\nCreating the dominoes of History");
 			List<Dominoes> dominoesPreviousHistory = DominoesSQLDao2.loadAllMatrices(databaseName, jTextField1.getText(), "CPU", previousHistory);
 
+			//multiplicando as matrizes e gerando a confidence
+			Dominoes domCF = dominoesPreviousHistory.get(6);
+			Dominoes domCFt = domCF.cloneNoMatrix();
+			domCFt.transpose();
+			Dominoes domFF = domCFt.multiply(domCF);
+			domFF.confidence();
+			
+			MergeFilesDao mergeFilesDao = new MergeFilesDao();
+			MergeFiles mergeFiles = mergeFilesDao.getMerge(mergesList.getSelectedItem().toString().split(" ")[0], repo.getProject());
+
+
+			EditedFilesDao filesDao = new EditedFilesDao();
+			mergeFiles.setFilesOnBranchOne(filesDao.getFiles(mergeFiles.getHashBase(), 
+																		mergeFiles.getParents()[0], 
+																		mergeFiles.getPath(),
+																		".java"));
+			mergeFiles.setFilesOnBranchTwo(filesDao.getFiles(mergeFiles.getHashBase(), 
+																		mergeFiles.getParents()[1], 
+																		mergeFiles.getPath(),
+																		".java"));
+			
+			Set<EditedFile> filesEdited =  new HashSet<>(mergeFiles.getFilesOnBranchOne());
+			filesEdited.addAll(filesEdited);
+			
+			printDominoes(domFF, (double) spinnerThreshold.getValue(), filesEdited);
+			
 		} catch (SQLException ex) {
 			Logger.getLogger(JFrameDependencies.class.getName()).log(Level.SEVERE, null, ex);
 		} catch (Exception ex) {
@@ -226,10 +289,57 @@ public class JFrameDependencies extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JComboBox mergesList;
+    private javax.swing.JSpinner spinnerThreshold;
     // End of variables declaration//GEN-END:variables
+
+	private void printDominoes(Dominoes dominoes, double threshold, Collection<EditedFile> filesEdited) {
+		
+		//System.out.println("getDevice\t" + dominoes.getDevice());
+		//System.out.println("getHistoric\t" + dominoes.getHistoric().toString());
+	
+		IMatrix2D matrix = dominoes.getMat();
+
+		//System.out.println("getColType\t" + matrix.getMatrixDescriptor().getColType());
+		//System.out.println("getRowType\t" + matrix.getMatrixDescriptor().getRowType());
+		//System.out.println("getNumRows\t" + matrix.getMatrixDescriptor().getNumRows());
+		//System.out.println("getNumCols\t" + matrix.getMatrixDescriptor().getNumCols());
+		
+		List<Cell> cells = matrix.getNonZeroData();
+		int rows = matrix.getMatrixDescriptor().getNumRows();
+		int cols = matrix.getMatrixDescriptor().getNumCols();
+		
+		for(int i = 0 ; i < rows ; i++){
+			
+			EditedFile efTemp = new EditedFile(matrix.getMatrixDescriptor().getRowAt(i));
+			
+			if(filesEdited.contains(efTemp)){
+			
+				System.out.println(matrix.getMatrixDescriptor().getRowAt(i));
+
+				for(int j = 0 ; j < cols ; j++){
+
+					if(i != j)
+
+						for(Cell c : cells){
+							if((c.value >= threshold) && (c.row == i) && (c.col == j))
+								System.out.println("\t" + c.value + "\t" + matrix.getMatrixDescriptor().getColumnAt(j));
+
+						}
+
+				}System.out.println();
+			
+			}
+
+		}
+		
+	}
+
 }
