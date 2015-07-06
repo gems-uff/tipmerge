a5c2fe1ef8881e1094f8a2971be4cc113bd6ca29
Catarina Costa <catarinasouzacosta@gmail.com>
2015-07-06 16:08:18 -0300

    New database: Voldemort, Caminho, Sapos and Derby. Set different parameters to run experiment.

--- src/br/uff/ic/gems/tipmerge/experiment/Parameter.java
+++ src/br/uff/ic/gems/tipmerge/experiment/Parameter.java
@@ -11,6 +11,9 @@ package br.uff.ic.gems.tipmerge.experiment;
  */
 public class Parameter {
 	public static double THRESHOLD = 0.6;
-	public static String EXTENSION = "java";	
+	//public static double THRESHOLD = 0.8;
+	//public static double THRESHOLD = 1.0;
+	//public static String EXTENSION = "All Files";
+	public static String EXTENSION = ".java";
 	public static String DATABASE = "data/gitdataminer.sqlite";
 }
