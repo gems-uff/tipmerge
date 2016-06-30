591790f1fce31e098baddea45a64788dd865ec64
Jair Figueiredo <jairfigueiredo@gmail.com>
2016-06-29 20:51:39 -0500

    include class to compare last commit to merge

--- src/br/uff/ic/gems/tipmerge/model/Committer.java
+++ src/br/uff/ic/gems/tipmerge/model/Committer.java
@@ -21,8 +21,8 @@ public class Committer implements Comparable<Committer> {
     private double zScoreM;
 
     public Committer(String name, String email) {
-        this.name = name;
-        this.email = email;
+        this.name = name.toLowerCase();
+        this.email = email.toLowerCase();
     }
 
     /**
